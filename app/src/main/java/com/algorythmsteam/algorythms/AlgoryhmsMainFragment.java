84b3a23c20634b1b82fc93acaf7b826eec45dcc9
galzo <galzohar@gmail.com>
2015-06-14 03:20:11 +0300

    redesigned splash screen to support the new NFC functionality, wrote new splash screen animations, replaced AnimationHandler with newer version

--- app/src/main/java/com/algorythmsteam/algorythms/AlgoryhmsMainFragment.java
+++ app/src/main/java/com/algorythmsteam/algorythms/AlgoryhmsMainFragment.java
@@ -3,9 +3,13 @@ package com.algorythmsteam.algorythms;
 import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.graphics.Point;
+import android.graphics.Typeface;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,11 +21,17 @@ import android.view.View.OnClickListener;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.AnticipateInterpolator;
 import android.view.animation.AnticipateOvershootInterpolator;
 import android.view.animation.BounceInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.OvershootInterpolator;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -39,8 +49,8 @@ public class AlgoryhmsMainFragment extends Fragment
     @Override
     public void onAnimationEnd(Animator animation) {
         //used after all animations have been displayed
-        scanButton.setOnClickListener(AlgoryhmsMainFragment.this);
-        buzzerButton.setOnClickListener(AlgoryhmsMainFragment.this);
+        nfcButton.setOnClickListener(AlgoryhmsMainFragment.this);
+        qrButton.setOnClickListener(AlgoryhmsMainFragment.this);
         aboutButton.setOnClickListener(AlgoryhmsMainFragment.this);
         isAnimInit = true;
     }
@@ -64,7 +74,8 @@ public class AlgoryhmsMainFragment extends Fragment
     private boolean isAnimInit;
     private MainActivityCallback activityCallback;
     private ImageView splashIcon, backgroundGrid;
-    private ImageButton scanButton, buzzerButton, aboutButton;
+    private ImageButton nfcButton, qrButton, aboutButton;
+    private TextView nfcDescription, qrDescription;
 
     public AlgoryhmsMainFragment() {
         super();
@@ -91,20 +102,25 @@ public class AlgoryhmsMainFragment extends Fragment
         root = inflater.inflate(R.layout.fragment_algoryhms_main, container, false);
         splashIcon = (ImageView) root.findViewById(R.id.splash_screen_app_icon);
         backgroundGrid = (ImageView) root.findViewById(R.id.splash_screen_background_grid);
-        scanButton = (ImageButton) root.findViewById(R.id.splash_screen_scan_button);
-        buzzerButton = (ImageButton) root.findViewById(R.id.splash_screen_buzzer_button);
+        nfcButton = (ImageButton) root.findViewById(R.id.splash_screen_nfc_scan_button);
+        qrButton = (ImageButton) root.findViewById(R.id.splash_screen_qr_scan_button);
         aboutButton = (ImageButton) root.findViewById(R.id.splash_screen_about_button);
+        nfcDescription = (TextView) root.findViewById(R.id.splash_screen_nfc_description);
+        qrDescription = (TextView) root.findViewById(R.id.splash_screen_qr_description);
 
+        Typeface tf = Typeface.createFromAsset(getActivity().getAssets(), "fonts/coopbl.TTF");
+        nfcDescription.setTypeface(tf);
+        qrDescription.setTypeface(tf);
         return root;
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        initAnims();
+        initSplashAnims();
     }
 
-    public void initAnims() {
+    public void initSplashAnims() {
         //if anim was already initialized - do not replay it
         if (isAnimInit) {
             return;
@@ -122,94 +138,151 @@ public class AlgoryhmsMainFragment extends Fragment
         float yOffset = ((AlgoryhmsMainActivity) getActivity()).convertDpToPixel(300);
 
         //set buttons animations
-        ObjectAnimator scanButtonUp = AnimationHandler.generateYAnimation(scanButton, yOffset, 0, 1400, 0, new AnticipateOvershootInterpolator());
-        ObjectAnimator scanButtonFadeIn = AnimationHandler.generateAlphaAnimation(scanButton, 0, 1, 10, 0, null);
+        ObjectAnimator qrButtonUp = AnimationHandler.generateYAnimation(qrButton, yOffset, 0, 1400, 0, new AnticipateOvershootInterpolator());
+        ObjectAnimator qrButtonFadeIn = AnimationHandler.generateAlphaAnimation(qrButton, 0, 1, 10, 0, null);
 
-        ObjectAnimator buzzerButtonUp = AnimationHandler.generateYAnimation(buzzerButton, yOffset, 0, 1400, 120, new AnticipateOvershootInterpolator());
-        ObjectAnimator buzzerButtonFadeIn = AnimationHandler.generateAlphaAnimation(buzzerButton, 0, 1, 10, 120, null);
+        ObjectAnimator nfcButtonUp = AnimationHandler.generateYAnimation(nfcButton, yOffset, 0, 1400, 120, new AnticipateOvershootInterpolator());
+        ObjectAnimator nfcButtonFadeIn = AnimationHandler.generateAlphaAnimation(nfcButton, 0, 1, 10, 120, null);
 
         ObjectAnimator aboutButtonUp = AnimationHandler.generateYAnimation(aboutButton, yOffset, 0, 1400, 240, new AnticipateOvershootInterpolator());
         ObjectAnimator aboutButtonFadeIn = AnimationHandler.generateAlphaAnimation(aboutButton, 0, 1, 10, 240, null);
         aboutButtonUp.addListener(this);
 
         AnimatorSet as = new AnimatorSet();
-        as.play(scanButtonUp).with(scanButtonFadeIn).with(buzzerButtonFadeIn).with(buzzerButtonUp)
+        as.play(qrButtonUp).with(qrButtonFadeIn).with(nfcButtonFadeIn).with(nfcButtonUp)
                 .with(aboutButtonFadeIn).with(aboutButtonUp).after(splashIconSpring);
         as.start();
     }
 
-    private void handleButtonClickTransition(int clickedButtonId) {
-        switch (clickedButtonId) {
-            case R.id.splash_screen_scan_button:
-                if (activityCallback != null) {
-                    activityCallback.initiateScan();
-                }
-
-                break;
-
-            case R.id.splash_screen_buzzer_button:
-                if (activityCallback != null) {
-                    activityCallback.openBuzzer();
-                }
-                break;
-
-            case R.id.splash_screen_about_button:
-                //TODO: not going to get here, add about button functionality to fix
-                Toast.makeText(getActivity(), "coming soon", Toast.LENGTH_LONG).show();
-                break;
-
-            default:
-                break;
-        }
-    }
-
-    private void handleTransitionAnim(final int clickedButtonId) {
-        float yOffset = ((AlgoryhmsMainActivity) getActivity()).convertDpToPixel(600);
-        ObjectAnimator splashIconUp = AnimationHandler.generateYAnimation(splashIcon, 0, -yOffset, 1400, 0, new AccelerateDecelerateInterpolator());
-        ObjectAnimator aboutButtonAnim = AnimationHandler.generateYAnimation(aboutButton, 0, yOffset, 1400, 0, new AccelerateDecelerateInterpolator());
-        aboutButtonAnim.addListener(new Animator.AnimatorListener() {
+//    private void handleButtonClickTransition(int clickedButtonId) {
+//        switch (clickedButtonId) {
+//            case R.id.splash_screen_nfc_scan_button:
+//                if (activityCallback != null) {
+//                    activityCallback.initiateScan();
+//                }
+//
+//                break;
+//
+//            case R.id.splash_screen_qr_scan_button:
+//                if (activityCallback != null) {
+//                    activityCallback.openBuzzer();
+//                }
+//                break;
+//
+//            case R.id.splash_screen_about_button:
+//                //TODO: not going to get here, add about button functionality to fix
+//                Toast.makeText(getActivity(), "coming soon", Toast.LENGTH_LONG).show();
+//                break;
+//
+//            default:
+//                break;
+//        }
+//    }
 
 
-            @Override
-            public void onAnimationStart(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                handleButtonClickTransition(clickedButtonId);
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-
-            }
-        });
-
-        ObjectAnimator nonClickedButtonAnim;
-        if (clickedButtonId == R.id.splash_screen_scan_button) {
-            nonClickedButtonAnim = AnimationHandler.generateYAnimation(buzzerButton, 0, yOffset, 1400, 0, new AccelerateDecelerateInterpolator());
-        } else {
-            nonClickedButtonAnim = AnimationHandler.generateYAnimation(scanButton, 0, -yOffset, 1400, 0, new AccelerateDecelerateInterpolator());
-        }
-
-        AnimatorSet as = new AnimatorSet();
-        as.play(splashIconUp).with(aboutButtonAnim).with(nonClickedButtonAnim);
-        as.start();
-
-    }
+//    private void handleTransitionAnim(final int clickedButtonId) {
+//        float yOffset = ((AlgoryhmsMainActivity) getActivity()).convertDpToPixel(600);
+//        ObjectAnimator splashIconUp = AnimationHandler.generateYAnimation(splashIcon, 0, -yOffset, 1400, 0, new AccelerateDecelerateInterpolator());
+//        ObjectAnimator aboutButtonAnim = AnimationHandler.generateYAnimation(aboutButton, 0, yOffset, 1400, 0, new AccelerateDecelerateInterpolator());
+//        aboutButtonAnim.addListener(new Animator.AnimatorListener() {
+//
+//
+//            @Override
+//            public void onAnimationStart(Animator animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationEnd(Animator animation) {
+//                handleButtonClickTransition(clickedButtonId);
+//            }
+//
+//            @Override
+//            public void onAnimationCancel(Animator animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationRepeat(Animator animation) {
+//
+//            }
+//        });
+//
+//        ObjectAnimator nonClickedButtonAnim;
+//        if (clickedButtonId == R.id.splash_screen_nfc_scan_button) {
+//            nonClickedButtonAnim = AnimationHandler.generateYAnimation(qrButton, 0, yOffset, 1400, 0, new AccelerateDecelerateInterpolator());
+//        } else {
+//            nonClickedButtonAnim = AnimationHandler.generateYAnimation(nfcButton, 0, -yOffset, 1400, 0, new AccelerateDecelerateInterpolator());
+//        }
+//
+//        AnimatorSet as = new AnimatorSet();
+//        as.play(splashIconUp).with(aboutButtonAnim).with(nonClickedButtonAnim);
+//        as.start();
+//    }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.splash_screen_about_button) {
-            Toast.makeText(getActivity(), "coming soon", Toast.LENGTH_LONG).show();
-        } else {
-            handleTransitionAnim(v.getId());
-        }
+        handleButtonClickAnimations(v.getId());
     }
+
+    private void handleButtonClickAnimations(final int clickedButtonID) {
+        ArrayList<Animator> animators = new ArrayList<>();
+        if (clickedButtonID == R.id.splash_screen_about_button) {
+            if (qrDescription.getAlpha() != 0) return;
+
+            animators.add(AnimationHandler.generatePopInAnimation(qrDescription, 0, 1, 500, 0, new OvershootInterpolator()));
+            animators.add(AnimationHandler.generatePopInAnimation(nfcDescription, 0, 1, 500, 120, new OvershootInterpolator()));
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    ArrayList<Animator> animators = new ArrayList<>();
+                    animators.add(AnimationHandler.generatePopOutAnimation(qrDescription, 1, 0, 500, 0, new AnticipateInterpolator()));
+                    animators.add(AnimationHandler.generatePopOutAnimation(nfcDescription, 1, 0, 500, 120, new AnticipateInterpolator()));
+                    AnimatorSet as = new AnimatorSet();
+                    as.playTogether(animators);
+                    as.start();
+                }
+            }, 1500);
+        }
+
+        else {
+            animators.add(AnimationHandler.generatePopOutAnimation(splashIcon, 1, 0, 500, 0, new AnticipateInterpolator()));
+            animators.add(AnimationHandler.generateAlphaAnimation(backgroundGrid, 0.3f, 0, 1200, 0, null));
+            if (clickedButtonID == R.id.splash_screen_qr_scan_button) {
+                int toMove = calcHorizontalCenterPoint(qrButton);
+                animators.add(AnimationHandler.generateXAnimation(qrButton, 0, toMove, 500, 360, new DecelerateInterpolator()));
+                animators.add(AnimationHandler.generatePopOutAnimation(nfcButton, 1, 0, 500, 120, new AnticipateInterpolator()));
+                animators.add(AnimationHandler.generatePopOutAnimation(aboutButton, 1, 0, 500, 240, new AnticipateInterpolator()));
+            } else if (clickedButtonID == R.id.splash_screen_nfc_scan_button) {
+                animators.add(AnimationHandler.generatePopOutAnimation(qrButton, 1, 0, 500, 120, new AnticipateInterpolator()));
+                animators.add(AnimationHandler.generatePopOutAnimation(aboutButton, 1, 0, 500, 240, new AnticipateInterpolator()));
+            }
+
+            View clickedButton = root.findViewById(clickedButtonID);
+            float yOffset = ((AlgoryhmsMainActivity) getActivity()).convertDpToPixel(300);
+            ObjectAnimator clickedButtonYTransition = AnimationHandler.generateYAnimation(clickedButton, 0, yOffset, 700, 1200, new AnticipateInterpolator());
+            animators.add(clickedButtonYTransition);
+        }
+
+        AnimatorSet as = new AnimatorSet();
+        as.playTogether(animators);
+        as.setStartDelay(50);
+        as.start();
+    }
+
+    private int calcHorizontalCenterPoint(View v) {
+        int[] viewLocation = new int[2];
+        v.getLocationOnScreen(viewLocation);
+
+        Display display = getActivity().getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int screenWidthMid = (size.x / 2);
+        int viewWidthMid = (v.getWidth() / 2);
+        int xOffset = Math.abs(viewLocation[0] - screenWidthMid);
+        return Math.abs(xOffset - viewWidthMid);
+    }
+
+
 }
--- app/src/main/java/com/handlers/AnimationHandler.java
+++ app/src/main/java/com/handlers/AnimationHandler.java
@@ -1,13 +1,33 @@
 package com.handlers;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.TimeInterpolator;
 import android.view.View;
+import android.view.animation.AnimationSet;
+
+import java.util.ArrayList;
 
 public class AnimationHandler {
 
+    /**
+     * Generates a single animation that runs on the given view
+     * @param v - the view to run animation on
+     * @param property - the property whose values are changed for animating the view
+     * @param firstVal - initial property value
+     * @param secondVal - changed propery value
+     * @param duration - animation's duration
+     * @param startDelay - animation's start delay
+     * @param interpolator - animations Timeinterpolator, provide NULL for default interpolator
+     * @return The required ObjectAnimator
+     */
     public static ObjectAnimator generateAnimation(View v, String property, float firstVal, float secondVal,
                                                    int duration, int startDelay, TimeInterpolator interpolator) {
+        if (v == null) {
+            return null;
+        }
+
         ObjectAnimator anim = ObjectAnimator.ofFloat(v, property, firstVal, secondVal);
         anim.setDuration(duration);
         anim.setStartDelay(startDelay);
@@ -16,6 +36,27 @@ public class AnimationHandler {
         }
 
         return anim;
+    }
+
+    /**
+     * Generate multiple animations with the same effect and properties on multiple views
+     * @return animatorSet which plays all animations together upon running it
+     */
+    public static AnimatorSet generateMultipleAnims(ArrayList<View> viewsList, String property, float firstVal, float secondVal,
+                                                     int duration, int startDelay, TimeInterpolator interpolator) {
+        AnimatorSet as = new AnimatorSet();
+        ArrayList<Animator> animators = new ArrayList<Animator>();
+        for (View v : viewsList) {
+            animators.add(generateAnimation(v, property, firstVal, secondVal, duration, startDelay, interpolator));
+        }
+
+        as.playTogether(animators);
+        return as;
+    }
+
+    public static ObjectAnimator generateXAnimation(View v, float firstVal, float secondVal,
+                                                    int duration, int startDelay, TimeInterpolator interpolator) {
+        return generateAnimation(v, "translationX", firstVal, secondVal, duration, startDelay, interpolator);
     }
 
     public static ObjectAnimator generateYAnimation(View v, float firstVal, float secondVal,
@@ -27,4 +68,71 @@ public class AnimationHandler {
                                                         int duration, int startDelay, TimeInterpolator interpolator) {
         return generateAnimation(v, "alpha", firstVal, secondVal, duration, startDelay, interpolator);
     }
+
+    public static ObjectAnimator generateXScaleAnimation(View v, float firstVal, float secondVal,
+                                                        int duration, int startDelay, TimeInterpolator interpolator) {
+        return generateAnimation(v, "scaleX", firstVal, secondVal, duration, startDelay, interpolator);
+    }
+
+    public static ObjectAnimator generateYScaleAnimation(View v, float firstVal, float secondVal,
+                                                         int duration, int startDelay, TimeInterpolator interpolator) {
+        return generateAnimation(v, "scaleY", firstVal, secondVal, duration, startDelay, interpolator);
+    }
+
+    public static AnimatorSet generatePopInAnimation(View v, float firstVal, float secondVal, int duration, int startDelay, TimeInterpolator interpolator) {
+        ObjectAnimator popX = generateXScaleAnimation(v, firstVal, secondVal, duration, 0, interpolator);
+        ObjectAnimator popY = generateYScaleAnimation(v, firstVal, secondVal, duration, 0, interpolator);
+        ObjectAnimator fadeIn = generateAlphaAnimation(v, 0, 1, 30, startDelay, null);
+        AnimatorSet as = new AnimatorSet();
+        as.play(fadeIn).with(popX).with(popY);
+        return as;
+    }
+
+    public static AnimatorSet generatePopOutAnimation(View v, float firstVal, float secondVal, int duration, int startDelay, TimeInterpolator interpolator) {
+        ObjectAnimator popX = generateXScaleAnimation(v, firstVal, secondVal, duration, 0, interpolator);
+        ObjectAnimator popY = generateYScaleAnimation(v, firstVal, secondVal, duration, 0, interpolator);
+        ObjectAnimator fadeOut = generateAlphaAnimation(v, 1, 0, duration, startDelay, null);
+        AnimatorSet as = new AnimatorSet();
+        as.play(fadeOut).with(popX).with(popY);
+        return as;
+    }
+
+    public static AnimatorSet generateMultipleYAnims(ArrayList<View> viewsList, float firstVal, float secondVal,
+                                                         int duration, int startDelay, TimeInterpolator interpolator) {
+        return generateMultipleAnims(viewsList, "translationY", firstVal, secondVal, duration, startDelay, interpolator);
+    }
+
+    public static AnimatorSet generateMultipleAlphaAnims(ArrayList<View> viewsList, float firstVal, float secondVal,
+                                                         int duration, int startDelay, TimeInterpolator interpolator) {
+        return generateMultipleAnims(viewsList, "alpha", firstVal, secondVal, duration, startDelay, interpolator);
+    }
+
+    public static AnimatorSet generateMultiplePopInAnims(ArrayList<View> viewsList, float firstVal, float secondVal,
+                                                         int duration, int startDelay, TimeInterpolator interpolator) {
+        ArrayList<Animator> popAnims = new ArrayList<Animator>();
+        for (View v : viewsList) {
+            popAnims.add(generatePopInAnimation(v, firstVal, secondVal, duration, startDelay, interpolator));
+        }
+
+        AnimatorSet as = new AnimatorSet();
+        as.playTogether(popAnims);
+        return as;
+    }
+
+    public static AnimatorSet generateMultipleAnimsOnObject(View v, ArrayList<String> props, float firstVal, float secondVal, int duration, int startDelay, TimeInterpolator interpolator) {
+        ArrayList<Animator> animators = new ArrayList<Animator>();
+        AnimatorSet as = null;
+        for (String prop : props) {
+            ObjectAnimator objAnimator = generateAnimation(v, prop, firstVal, secondVal, duration, 0, interpolator);
+            animators.add(objAnimator);
+        }
+        if (animators.size() > 0) {
+            animators.get(0).setStartDelay(startDelay);
+            as = new AnimatorSet();
+            as.playTogether(animators);
+        }
+
+        return as;
+    }
+
 }
